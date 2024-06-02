01d4926d91d19122da78e540902ff9231716a6a2
SerhoBanch <anton.sergienko32@gmail.com>
2024-06-02 13:59:08 +0300

    Add files via upload

---
+++ Cache/UserCache.cs
@@ -0,0 +1,16 @@
+﻿using MyDiary.Models;
+
+namespace MyDiary.Cache
+{
+    class UserCache
+    {
+        #region Реализация паттерна Singleton
+        private static readonly UserCache instance = new();
+        public static UserCache Instance => instance;
+        private UserCache() { }
+        #endregion
+
+        public User CurrentUser { get; set; } = null;
+
+    }
+}
